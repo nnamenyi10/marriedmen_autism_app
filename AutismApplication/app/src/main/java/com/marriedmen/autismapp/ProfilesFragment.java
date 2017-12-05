e6e1cab502c40853371c57cd9023b9d79ce2e2af
Jason Griffith <jason@dr.facestab@live.com>
2017-12-04 20:52:43 -0500

    take two...

--- AutismApplication/app/src/main/java/com/marriedmen/autismapp/ProfilesFragment.java
+++ AutismApplication/app/src/main/java/com/marriedmen/autismapp/ProfilesFragment.java
@@ -37,7 +37,7 @@ public class ProfilesFragment extends ListFragment
         //SQLiteDatabase db = this.getReadableDatabase();
 
         DBHelper db = new DBHelper(getActivity());
-        
+
 
         // test array
         String[] test = new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
